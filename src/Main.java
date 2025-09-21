05958b48fa71d762eef5178ea1bace98c8f36a0a
JoanNnadi <nnadijn@mail.uc.edu>
2025-09-20 23:43:52 -0400

    Task 4 completed

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,26 @@ public class Main
     {
 
        Scanner in = new Scanner(System.in);
-       int  Age;
+       int  Age ;
+        System.out.print("please enter your age: ");
+        Age = in.nextInt();
+
+        if (Age >= 21)
+        {
+            System.out.println(" You get a paper wrist band: ");
+            System.out.println(" your age is 21 or older: ");
+        }
+        else if (Age !=21)
+
+        {
+           System.out.println("you do not get a wrist band: " );
+           System.out.println(" you are not 21 years old: ");
+        }
+        else
+        {
+            System.out.println("No paper wrist band for you: ");
+            System.out.println(" you are not 21 years or older: ");
+        }
+
     }
 }
